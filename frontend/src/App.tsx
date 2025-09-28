66472571514d0c14e8b4206be54c795f30800214
Ayushmaan Gupta <ayush.261.gupta@gmail.com>
2025-09-28 18:54:57 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
