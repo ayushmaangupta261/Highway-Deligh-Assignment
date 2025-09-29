35143cf4f9b3af3fef9147819a202bbe2647cb37
Ayushmaan Gupta <ayush.261.gupta@gmail.com>
2025-09-29 23:04:07 +0530

    App updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,46 +1,50 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AuthPage } from './pages/AuthPage';
-import SplashScreen from "./pages/SplashScreen";
-import { Toaster } from "react-hot-toast";
+import AuthPage from './pages/AuthPage';
+import SplashScreen from './pages/SplashScreen';
 import Dashboard from './pages/Dashboard';
-import ProtectedRoute from './component/Auth/ProtectedRoute';
-import Navbar from './component/Navigation/Navbar';
-import UnProtectedRoute from './component/Auth/UnProtectedRoute';
-
+import ProtectedRoute from './components/Auth/ProtectedRoute';
+import UnProtectedRoute from './components/Auth/UnProtectedRoute';
+import Navbar from './components/Navigation/Navbar';
+import { Toaster } from 'react-hot-toast';
 
 const App: React.FC = () => {
   return (
-    <div className='overflow-hidden relative'>
+    <div className="overflow-hidden relative">
 
       <div className="absolute top-4 left-1/2 transform -translate-x-1/2 w-[80%] z-50">
         <Navbar />
       </div>
 
-
       <Toaster position="top-right" reverseOrder={false} />
 
       <Routes>
-        <Route path="/"
+        <Route 
+          path="/" 
           element={
             <UnProtectedRoute>
               <SplashScreen />
             </UnProtectedRoute>
-          } />
+          } 
+        />
 
-        <Route path="/authPage"
+        <Route 
+          path="/authPage" 
           element={
             <UnProtectedRoute>
               <AuthPage />
             </UnProtectedRoute>
-          } />
+          } 
+        />
 
-        <Route path="/dashboard"
+        <Route 
+          path="/dashboard" 
           element={
             <ProtectedRoute>
               <Dashboard />
             </ProtectedRoute>
-          } />
+          } 
+        />
       </Routes>
     </div>
   );
