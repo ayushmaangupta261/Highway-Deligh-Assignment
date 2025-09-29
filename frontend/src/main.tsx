7695c86f675206fad20cff6e7ed7270acb7f9457
Ayushmaan Gupta <ayush.261.gupta@gmail.com>
2025-09-30 03:40:17 +0530

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,7 @@ const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter >
+  <BrowserRouter basename="/Highway-Delight-Assignment">
     <Provider store={store}>
       <GoogleOAuthProvider clientId={clientId}>
         <App />
